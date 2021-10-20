18d0f10ec0155a59add81b0f598d3d18990baa16
Wu Wenhao <1321471639@qq.com>
2021-10-20 16:53:10 +0800

    add files

---
+++ vendor/composer/installed.php
@@ -0,0 +1,33 @@
+<?php return array (
+  'root' => 
+  array (
+    'pretty_version' => 'dev-main',
+    'version' => 'dev-main',
+    'aliases' => 
+    array (
+    ),
+    'reference' => 'ad60bbec4ba7b9a992e52452cb7c3249e0ba8694',
+    'name' => '__root__',
+  ),
+  'versions' => 
+  array (
+    '__root__' => 
+    array (
+      'pretty_version' => 'dev-main',
+      'version' => 'dev-main',
+      'aliases' => 
+      array (
+      ),
+      'reference' => 'ad60bbec4ba7b9a992e52452cb7c3249e0ba8694',
+    ),
+    'workerman/workerman' => 
+    array (
+      'pretty_version' => 'v4.0.20',
+      'version' => '4.0.20.0',
+      'aliases' => 
+      array (
+      ),
+      'reference' => 'ab9a28b03c34549525fac1cc170f6e3b65e047e6',
+    ),
+  ),
+);
